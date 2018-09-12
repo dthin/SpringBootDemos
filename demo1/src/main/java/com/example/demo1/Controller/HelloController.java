4f88cec7906f3db829ae76d5b1fa5ed0e39f1ace
dthin <925808869@qq.com>
2018-09-13 00:21:37 +0800

    first commit

---
+++ demo1/src/main/java/com/example/demo1/Controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo1.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("hello")
+    String hello(){
+        return "Hello";
+    }
+
+
+}
